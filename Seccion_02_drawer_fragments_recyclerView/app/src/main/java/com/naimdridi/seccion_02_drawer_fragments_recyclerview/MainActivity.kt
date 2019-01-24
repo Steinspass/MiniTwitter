3a6dbca3ebb45df199834d42118dc160b463f6f7
Steinspass <37332713+Steinspass@users.noreply.github.com>
2019-01-24 18:20:50 +0100

    Add library, toolbar, nav_header and menu_drawer

--- Seccion_02_drawer_fragments_recyclerView/app/src/main/java/com/naimdridi/seccion_02_drawer_fragments_recyclerview/MainActivity.kt
+++ Seccion_02_drawer_fragments_recyclerView/app/src/main/java/com/naimdridi/seccion_02_drawer_fragments_recyclerview/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.naimdridi.seccion_02_drawer_fragments_recyclerview
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import com.naimdridi.my_library_second.Interfaces.ToolbarActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ToolbarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbarToLoad(toolbar as Toolbar)
     }
 }
