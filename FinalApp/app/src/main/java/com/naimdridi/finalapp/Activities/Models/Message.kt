1b1302a7219c4ee2cdc9e1ce26814824965c423b
Steinspass <37332713+Steinspass@users.noreply.github.com>
2019-02-09 17:41:39 +0100

    Create bubbleChat, model Message and layout message chat

---
+++ FinalApp/app/src/main/java/com/naimdridi/finalapp/Activities/Models/Message.kt
@@ -0,0 +1,5 @@
+package com.naimdridi.finalapp.Activities.Models
+
+import java.util.*
+
+data class Message(val authorId: String, val message: String, val profileImageUrl: String, val sentAt: Date)
